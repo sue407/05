bbbb47749da996ec334ba523461f2408eba938b4
sue407 <sue407407@gmail.com>
2022-10-04 17:06:24 +0900

    (3)
    10/4 (3)

--- main.c
+++ main.c
@@ -4,13 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	char c;
+	int count=0;
+	printf("input a string: " );
+	scanf("%c", &c);
+
+	while ( (c = getchar()) !='\n') // 입력문자가 개행문자가 나올깨까지 반복 
+	{
+		
+		if ( c>='0' && c<='9' ) //입력된 글자가 숫자인가?  
+		 count = count + 1; //그렇다면 하나를 센다. 
 	
-	int a;
-	printf("정수 하나를 입력하시오 : " );
-	scanf("%i", &a);
-	if (a>=0)
-	 printf("절대값은 %i 입니다.", a);
-	else
-	 printf("절대값은 %i 입니다.", -a);
+	 	
+	}
+    printf("the number of digits is %i \n", count);
 	return 0;
 }
